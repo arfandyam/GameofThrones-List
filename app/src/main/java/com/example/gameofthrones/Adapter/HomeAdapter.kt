4c307d9f76f89e05641c09993cca224c3bb07ea7
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2021-12-13 23:51:31 +0800

    update adapter for detail character

--- app/src/main/java/com/example/gameofthrones/Adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/gameofthrones/Adapter/HomeAdapter.kt
@@ -3,22 +3,31 @@ package com.example.gameofthrones.Adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gameofthrones.DataCharacterItem
 import com.example.gameofthrones.R
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.home_itemlist.view.*
 
-class HomeAdapter(private val listCharacter : ArrayList<DataCharacterItem>) : RecyclerView.Adapter<HomeAdapter.Homeholder>() {
+class HomeAdapter(private val listCharacter : ArrayList<DataCharacterItem>, val listener: OnItemClickListener) : RecyclerView.Adapter<HomeAdapter.Homeholder>() {
+
+    interface OnItemClickListener{
+        fun onClick(character: DataCharacterItem)
+    }
 
     class Homeholder(itemview: View) : RecyclerView.ViewHolder(itemview){
-        fun bind(character : DataCharacterItem){
+        fun bind(character : DataCharacterItem, listener: OnItemClickListener){
             with(itemView){
                 Picasso.get()
                     .load(character.imageUrl)
                     .into(character_img)
                 namatv.text = character.fullName
                 familytv.text = "Family: " + character.family
+
+                itemView.setOnClickListener{
+                    listener.onClick(character)
+                }
             }
         }
     }
@@ -28,7 +37,7 @@ class HomeAdapter(private val listCharacter : ArrayList<DataCharacterItem>) : Re
     }
 
     override fun onBindViewHolder(holder: HomeAdapter.Homeholder, position: Int) {
-        holder.bind(listCharacter[position])
+        holder.bind(listCharacter[position], listener)
     }
 
     override fun getItemCount(): Int {
