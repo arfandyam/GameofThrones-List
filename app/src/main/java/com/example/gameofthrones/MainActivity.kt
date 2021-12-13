99c539faed23b633c33a645e55603f0ec43605f0
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2021-12-13 21:58:48 +0800

    Update Main Activity

--- app/src/main/java/com/example/gameofthrones/MainActivity.kt
+++ app/src/main/java/com/example/gameofthrones/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.gameofthrones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.gameofthrones.UI.Home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment: Fragment = HomeFragment()
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container_fragment, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
