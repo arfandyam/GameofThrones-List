8acc9cebe641976d56832cab8ca3ba0ea2f0f930
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2021-12-13 21:57:50 +0800

    Adapter pengisian list recyclerview

---
+++ app/src/main/java/com/example/gameofthrones/Adapter/HomeAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.gameofthrones.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gameofthrones.DataCharacterItem
+import com.example.gameofthrones.R
+import kotlinx.android.synthetic.main.home_itemlist.view.*
+
+class HomeAdapter(private val listCharacter : ArrayList<DataCharacterItem>) : RecyclerView.Adapter<HomeAdapter.Homeholder>() {
+
+    class Homeholder(itemview: View) : RecyclerView.ViewHolder(itemview){
+        fun bind(character : DataCharacterItem){
+            with(itemView){
+                namatv.text = character.fullName
+                familytv.text = character.family
+            }
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapter.Homeholder {
+        val view = LayoutInflater.from(parent.context). inflate(R.layout.home_itemlist, parent, false)
+        return Homeholder(view)
+    }
+
+    override fun onBindViewHolder(holder: HomeAdapter.Homeholder, position: Int) {
+        holder.bind(listCharacter[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listCharacter.size
+    }
+
+}
